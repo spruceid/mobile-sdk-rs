10cde0fa5c2e5c02dd8b44c8931560fec9c9f1b4
Simon Bihel <simon.bihel@spruceid.com>
2024-02-29 12:46:42 +0000

    Add SpruceID prefix to packages names

--- Package.swift
+++ Package.swift
@@ -5,22 +5,22 @@
 import PackageDescription;
 
 let package = Package(
-    name: "WalletSdkRs",
+    name: "SpruceIDWalletSdkRs",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15)
     ],
     products: [
         .library(
-            name: "WalletSdkRs",
-            targets: ["WalletSdkRs"]
+            name: "SpruceIDWalletSdkRs",
+            targets: ["SpruceIDWalletSdkRs"]
         )
     ],
     dependencies: [ ],
     targets: [
         .binaryTarget(name: "RustFramework", url: "https://github.com/spruceid/wallet-sdk-rs/releases/download/0.0.7/RustFramework.xcframework.zip", checksum: "77861db98e8c4de6f6bb64eef3cb058a6887939a4d72e4012e011c552a7c9d68"),
         .target(
-            name: "WalletSdkRs",
+            name: "SpruceIDWalletSdkRs",
             dependencies: [
                 .target(name: "RustFramework")
             ],
