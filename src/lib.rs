6849c96529de60180379199517908d5b5b10bdc2
Ryan Tate <ryan.michael.tate@gmail.com>
2024-08-19 18:35:32 -0700

    wip: handle oid4vp authorization request
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 uniffi::setup_scaffolding!();
 
 pub mod common;
+pub mod key_manager;
 pub mod local_store;
 pub mod mdl;
+pub mod metadata_manager;
 pub mod oid4vp;
 pub mod storage_manager;
+pub mod trust_manager;
 pub mod vdc_collection;
 pub mod wallet;
--- src/wallet.rs
+++ src/wallet.rs
@@ -1,12 +1,28 @@
-use crate::{common::Url, oid4vp::presentation_exchange::AuthorizationRequestObject};
+use crate::{
+    common::Url,
+    key_manager::KeyManager,
+    storage_manager::{self, StorageManagerInterface},
+    trust_manager::TrustManager,
+    vdc_collection::{VdcCollection, VdcCollectionError},
+};
 
 use std::sync::Arc;
 
+use anyhow::bail;
 use oid4vp::{
-    core::{authorization_request::verification::RequestVerifier, metadata::WalletMetadata},
+    core::{
+        authorization_request::{
+            verification::{did::verify_with_resolver, RequestVerifier},
+            AuthorizationRequestObject,
+        },
+        metadata::WalletMetadata,
+    },
+    presentation_exchange::{DescriptorMap, PresentationSubmission},
     wallet::Wallet as OID4VPWallet,
 };
 
+use ssi_dids::{DIDKey, VerificationMethodDIDResolver};
+use ssi_verification_methods::AnyJwkMethod;
 use thiserror::Error;
 
 /// The [WalletError] enum represents the errors that can occur
@@ -21,6 +37,12 @@ pub enum WalletError {
     OID4VPRequestValidation(String),
     #[error("Failed to resolve the presentation definition: {0}")]
     OID4VPPresentationDefinitionResolution(String),
+    #[error(transparent)]
+    Storage(#[from] storage_manager::StorageManagerError),
+    #[error(transparent)]
+    VdcCollection(#[from] VdcCollectionError),
+    #[error("Required credential not found for input descriptor id: {0}")]
+    RequiredCredentialNotFound(String),
 }
 
 // Handle unexpected errors when calling a foreign callback
@@ -53,6 +75,10 @@ impl From<uniffi::UnexpectedUniFFICallbackError> for WalletError {
 pub struct Wallet {
     client: oid4vp::core::util::ReqwestClient,
     metadata: WalletMetadata,
+    vdc_collection: VdcCollection,
+    trust_manager: TrustManager,
+    key_manager: Box<dyn KeyManager>,
+    storage_manager: Box<dyn StorageManagerInterface>,
 }
 
 #[uniffi::export]
@@ -67,13 +93,24 @@ impl Wallet {
     ///
     /// If the HTTP client fails to initialize.
     #[uniffi::constructor]
-    pub fn new() -> Result<Arc<Self>, WalletError> {
+    pub fn new(
+        storage_manager: Box<dyn StorageManagerInterface>,
+        key_manager: Box<dyn KeyManager>,
+    ) -> Result<Arc<Self>, WalletError> {
         let client = oid4vp::core::util::ReqwestClient::new()
             .map_err(|e| WalletError::HttpClientInitialization(format!("{e}")))?;
 
         Ok(Arc::new(Self {
             client,
+            // TODO: The wallet metadata should be retrieved from the device storage manager
+            // or fallback to the default oid4vp scheme.
+            // TODO: There should be an interface to support updated the wallet metadata
+            // dynamically. Replace with MetadataManager once ready.
             metadata: WalletMetadata::openid4vp_scheme_static(),
+            vdc_collection: VdcCollection::new(),
+            trust_manager: TrustManager::new(),
+            key_manager,
+            storage_manager,
         }))
     }
 
@@ -93,11 +130,109 @@ impl Wallet {
             .await
             .map_err(|e| WalletError::OID4VPPresentationDefinitionResolution(e.to_string()))?;
 
+        // Check if the verifiable credential(s) exists in the storage.
+        let credentials = presentation_definition
+            .parsed()
+            .input_descriptors()
+            .iter()
+            .map(|input_descriptor| {
+                match self
+                    .vdc_collection
+                    .get(input_descriptor.id(), &self.storage_manager)
+                {
+                    Ok(Some(credential)) => Ok(Some(credential)),
+                    Ok(None) => {
+                        // Check if the input descriptor contains required constraints.
+                        if input_descriptor.constraints().is_required() {
+                            Err(WalletError::RequiredCredentialNotFound(
+                                input_descriptor.id().to_string(),
+                            ))
+                        } else {
+                            Ok(None)
+                        }
+                    }
+                    Err(e) => Err(WalletError::VdcCollection(e)),
+                }
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        // Create a descriptor map for the presentation submission based on the credentials
+        // returned from the VDC collection.
+        let descriptor_map = credentials
+            .into_iter()
+            .enumerate()
+            .filter_map(|(index, credential)| match credential {
+                None => return None,
+                // Only create descriptor maps for credentials that exist in the storage.
+                Some(credential) => {
+                    // Create a descriptor map for the presentation submission.
+                    DescriptorMap::new(*credential.id(), credential.format().to_owned(), "$".into())
+                        // TODO: Determine if the nested path should be set.
+                        // For example:
+                        .set_path_nested(DescriptorMap::new(
+                            *credential.id(),
+                            credential.format().to_owned(),
+                            format!("$.vp.verifiableCredential[{index}]"),
+                        ))
+                        .ok()
+                }
+            })
+            .collect::<Vec<DescriptorMap>>();
+
+        // Create a presentation submission.
+        let presentation_submission = PresentationSubmission::new(
+            uuid::Uuid::new_v4(),
+            presentation_definition.parsed().id().clone(),
+            descriptor_map,
+        );
+
+        // Create a VP Token response.
+
         Ok(None)
     }
 }
 
-impl RequestVerifier for Wallet {}
+impl RequestVerifier for Wallet {
+    fn did<'life0, 'life1, 'async_trait>(
+        &'life0 self,
+        decoded_request: &'life1 AuthorizationRequestObject,
+        request_jwt: String,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = anyhow::Result<(), anyhow::Error>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        'life1: 'async_trait,
+        Self: ::core::marker::Sync + 'async_trait,
+    {
+        let trusted_dids = self
+            .trust_manager
+            .get_trusted_dids(&self.storage_manager)
+            .ok();
+
+        let wallet_metadata = self.metadata.clone();
+
+        let resolver: VerificationMethodDIDResolver<DIDKey, AnyJwkMethod> =
+            VerificationMethodDIDResolver::new(DIDKey);
+
+        Box::pin(async move {
+            verify_with_resolver(
+                &wallet_metadata,
+                decoded_request,
+                request_jwt,
+                trusted_dids.as_ref().map(|did| did.as_slice()),
+                resolver,
+            )
+            .await?;
+
+            Ok(())
+        })
+    }
+}
 
 impl OID4VPWallet for Wallet {
     type HttpClient = oid4vp::core::util::ReqwestClient;
