475aab37561ecde8e1bfa9bf21be64dfadde600f
Ryan Tate <ryan.michael.tate@gmail.com>
2024-08-27 10:04:14 -0700

    add common module for key, value, uuid, url, etc. types
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 uniffi::setup_scaffolding!();
 
+pub mod common;
 pub mod local_store;
 pub mod mdl;
 pub mod storage_manager;
 pub mod vdc_collection;
 pub mod w3c_vc_barcodes;
 
+pub use common::*;
 pub use mdl::*;
