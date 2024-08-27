3fd17d975c520e09bfd9b6c7e78a34a8ab07a44f
Ryan Tate <ryan.michael.tate@gmail.com>
2024-08-27 10:22:26 -0700

    add basic metadata manager implementation
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ uniffi::setup_scaffolding!();
 pub mod common;
 pub mod local_store;
 pub mod mdl;
+pub mod metadata_manager;
 pub mod storage_manager;
 pub mod vdc_collection;
 pub mod w3c_vc_barcodes;
