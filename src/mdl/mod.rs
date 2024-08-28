9f80466baff25ba98bda2b27ea9dbfb9a0d8c5ce
Ross Schulman <ross@rbs.io>
2024-08-28 10:35:20 -0400

    Move rest of presentation function and documentation

--- src/mdl/mod.rs
+++ src/mdl/mod.rs
@@ -1,9 +1,31 @@
-use crate::Wallet;
+//! A module to handle presentation of an ISO 18013-5 mobile driving license (mDL).
+//!
+//! This module manages the session for presentation of an mDL, including generating
+//! the QR code and BLE parameters, as well as managing the types of information requested
+//! by the reader.
+//!
+//! The presentation process is begun by calling [`initialize_mdl_presentation`] on the
+//! [`Wallet`] and passing the id of the mdoc to be used as well as a UUID that the client
+//! will use for the BLE central client:
+//!
+//! ```rust
+//! let ble_uuid = Uuid::new_v4();
+//! let presentation_session = wallet.initialize_mdl_presentation("test_mdoc", ble_uuid);
+//! // Presenting software then creates a QR code with presentation_session.qr_code_uri
+//! // and sets up a BLE session using presentation_session.ble_ident. It will then
+//! // receive a list of requested fields from the reader.
+//! let requested_items = presentation_session.handle_request(request_bytes)?;
+//! // Presenting software should then confirm with the user that they wish to present
+//! // the requested information. If so...
+//! let signed_response_bytes = presentation_session.submit_response(user_permitted_items, key_id)?;
+//! // Presenting software then sends response_bytes over BLE to the reader, completing the exchange.
+use crate::{KeyManagerInterface, Wallet};
 
 use super::common::*;
 
 use std::{
     collections::HashMap,
+    ops::DerefMut,
     sync::{Arc, Mutex},
 };
 
@@ -15,7 +37,6 @@ use isomdl::{
     },
     presentation::device::{self, Document, SessionManagerInit},
 };
-use ssi_claims::ResourceProvider;
 
 #[uniffi::export]
 impl Wallet {
@@ -40,7 +61,7 @@ impl Wallet {
     ) -> Result<MdlPresentationSession, SessionError> {
         let document = self
             .vdc_collection
-            .get(mdoc_id, &self.storage_manager)
+            .get(mdoc_id, self.storage_manager.clone())
             .map_err(|_| SessionError::Generic {
                 value: "Error in VDC Collection".to_string(),
             })?
@@ -75,8 +96,9 @@ impl Wallet {
             })?;
         Ok(MdlPresentationSession {
             state: Mutex::new(MdlPresentationState::Engaged),
+            key_manager: self.key_manager.clone(),
             engaged: Some(engaged_state),
-            in_process: None,
+            in_process: Mutex::new(None),
             qr_code_uri,
             ble_ident,
         })
@@ -87,9 +109,10 @@ impl Wallet {
 pub struct MdlPresentationSession {
     state: Mutex<MdlPresentationState>,
     engaged: Option<device::SessionManagerEngaged>,
-    in_process: Option<Mutex<InProcessRecord>>,
-    qr_code_uri: String,
-    ble_ident: String,
+    in_process: Mutex<Option<InProcessRecord>>,
+    pub qr_code_uri: String,
+    pub ble_ident: String,
+    key_manager: Arc<dyn KeyManagerInterface>,
 }
 
 #[derive(uniffi::Enum)]
@@ -98,24 +121,29 @@ pub enum MdlPresentationState {
     InProcess,
 }
 
-#[derive(uniffi::Object)]
+#[derive(uniffi::Object, Clone)]
 struct InProcessRecord {
     session: device::SessionManager,
-    items_request: Vec<ItemsRequest>,
+    items_request: device::RequestedItems,
 }
 
 #[uniffi::export]
 impl MdlPresentationSession {
-    pub fn handle_request(&self, request: Vec<u8>) -> Result<RequestData, RequestError> {
+    /// Handle a request from a reader that is seeking information from the mDL holder.
+    ///
+    /// Takes the raw bytes received from the reader by the holder over the transmission
+    /// technology. Returns a Vector of information items requested by the reader, or an
+    /// error.
+    pub fn handle_request(&self, request: Vec<u8>) -> Result<Vec<ItemsRequest>, RequestError> {
+        // Mutexes only return Err if another thread has panicked while holding the mutex
+        // If that has happened, its probably better to just crash. This is what the standard documentation recommends.
+        // See https://doc.rust-lang.org/std/sync/struct.Mutex.html
         let mut state = self.state.lock().unwrap();
         let (session_manager, items_requests) = {
             let session_establishment: SessionEstablishment = serde_cbor::from_slice(&request)
                 .map_err(|e| RequestError::Generic {
                     value: format!("Could not deserialize request: {e:?}"),
                 })?;
-            // Mutexes only return Err if another thread has panicked while holding the mutex
-            // If that has happened, its probably better to just crash. This is what the stdandard documentation recommends.
-            // See https://doc.rust-lang.org/std/sync/struct.Mutex.html
             if let MdlPresentationState::Engaged = *state {
                 // If the state is `Engaged`, then self.engaged must have been set at the same time, so safe to unwrap
                 self.engaged
@@ -134,27 +162,96 @@ impl MdlPresentationSession {
         };
 
         *state = MdlPresentationState::InProcess;
-        Ok(RequestData {
-            session_manager: Arc::new(SessionManager {
-                inner: Mutex::new(session_manager),
-                items_requests: items_requests.clone(),
-            }),
-            items_requests: items_requests
-                .into_iter()
-                .map(|req| ItemsRequest {
-                    doc_type: req.doc_type,
-                    namespaces: req
-                        .namespaces
-                        .into_inner()
-                        .into_iter()
-                        .map(|(ns, es)| {
-                            let items_request = es.into_inner().into_iter().collect();
-                            (ns, items_request)
-                        })
-                        .collect(),
-                })
-                .collect(),
-        })
+        let mut in_process = self.in_process.lock().unwrap();
+        *in_process = Some(InProcessRecord {
+            session: session_manager,
+            items_request: items_requests.clone(),
+        });
+        Ok(in_process
+            .as_ref()
+            .unwrap()
+            .items_request
+            .clone()
+            .into_iter()
+            .map(|req| ItemsRequest {
+                doc_type: req.doc_type,
+                namespaces: req
+                    .namespaces
+                    .into_inner()
+                    .into_iter()
+                    .map(|(ns, es)| {
+                        let items_request = es.into_inner().into_iter().collect();
+                        (ns, items_request)
+                    })
+                    .collect(),
+            })
+            .collect())
+    }
+
+    /// Constructs the response to be sent from the holder to the reader containing
+    /// the items of information the user has consented to share.
+    ///
+    /// Takes a HashMap of items the user has authorized the app to share, as well
+    /// as the id of a key stored in the key manager to be used to sign the response.
+    /// Returns a byte array containing the signed response to be returned to the
+    /// reader.
+    pub fn submit_response(
+        &self,
+        permitted_items: HashMap<String, HashMap<String, Vec<String>>>,
+        key_id: Key,
+    ) -> Result<Vec<u8>, SignatureError> {
+        let permitted = permitted_items
+            .into_iter()
+            .map(|(doc_type, namespaces)| {
+                let ns = namespaces.into_iter().collect();
+                (doc_type, ns)
+            })
+            .collect();
+        if let Some(ref mut in_process) = self.in_process.lock().unwrap().deref_mut() {
+            in_process
+                .session
+                .prepare_response(&in_process.items_request, permitted);
+            let response = in_process
+                .session
+                .get_next_signature_payload()
+                .map(|(_, payload)| payload)
+                .ok_or(SignatureError::MissingSignature)?
+                .to_vec();
+            let signed_response = self
+                .key_manager
+                .sign_payload(key_id, response)
+                .map_err(|e| SignatureError::Generic {
+                    value: format!("Error signing payload: {e:?}"),
+                })?;
+            in_process
+                .session
+                .submit_next_signature(signed_response)
+                .map_err(|e| SignatureError::Generic {
+                    value: format!("Could not submit next signature: {e:?}"),
+                })?;
+            in_process
+                .session
+                .retrieve_response()
+                .ok_or(SignatureError::TooManyDocuments)
+        } else {
+            Err(SignatureError::Generic {
+                value: "Could not get lock on session".to_string(),
+            })
+        }
+    }
+
+    /// Terminates the mDL exchange session.
+    ///
+    /// Returns the termination message to be transmitted to the reader.
+    pub fn terminate_session(&self) -> Result<Vec<u8>, TerminationError> {
+        let msg = session::SessionData {
+            data: None,
+            status: Some(session::Status::SessionTermination),
+        };
+        let msg_bytes = serde_cbor::to_vec(&msg).map_err(|e| TerminationError::Generic {
+            value: format!("Could not serialize message bytes: {e:?}"),
+        })?;
+        Ok(msg_bytes)
     }
 }
 
@@ -164,68 +261,22 @@ pub enum SessionError {
     Generic { value: String },
 }
 
-#[derive(uniffi::Object)]
-pub struct SessionManagerEngaged(device::SessionManagerEngaged);
-
-#[derive(uniffi::Record)]
-struct SessionData {
-    state: Arc<SessionManagerEngaged>,
-    qr_code_uri: String,
-    ble_ident: String,
-}
-
 #[derive(thiserror::Error, uniffi::Error, Debug)]
 pub enum RequestError {
     #[error("{value}")]
     Generic { value: String },
 }
 
-#[derive(uniffi::Record)]
-struct ItemsRequest {
+#[derive(uniffi::Record, Clone)]
+pub struct ItemsRequest {
     doc_type: String,
     namespaces: HashMap<String, HashMap<String, bool>>,
 }
 
-#[derive(uniffi::Object)]
-pub struct SessionManager {
-    inner: Mutex<device::SessionManager>,
-    items_requests: device::RequestedItems,
-}
-
-#[derive(uniffi::Record)]
-struct RequestData {
-    session_manager: Arc<SessionManager>,
-    items_requests: Vec<ItemsRequest>,
-}
-
-#[uniffi::export]
 #[derive(thiserror::Error, uniffi::Error, Debug)]
 pub enum ResponseError {
-    #[error("no signature payload received from session manager")]
-    MissingSignature,
     #[error("{value}")]
     Generic { value: String },
-}
-
-#[uniffi::export]
-fn submit_response(
-    session_manager: Arc<SessionManager>,
-    permitted_items: HashMap<String, HashMap<String, Vec<String>>>,
-) -> Result<Vec<u8>, ResponseError> {
-    let permitted = permitted_items
-        .into_iter()
-        .map(|(doc_type, namespaces)| {
-            let ns = namespaces.into_iter().collect();
-            (doc_type, ns)
-        })
-        .collect();
-    let mut session_manager_inner = session_manager.inner.lock().unwrap();
-    session_manager_inner.prepare_response(&session_manager.items_requests, permitted);
-    Ok(session_manager_inner
-        .get_next_signature_payload()
-        .map(|(_, payload)| payload)
-        .ok_or(ResponseError::MissingSignature)?
-        .to_vec())
 }
 
 #[derive(thiserror::Error, uniffi::Error, Debug)]
@@ -236,45 +287,14 @@ pub enum SignatureError {
     TooManyDocuments,
     #[error("{value}")]
     Generic { value: String },
-}
-
-#[uniffi::export]
-fn submit_signature(
-    session_manager: Arc<SessionManager>,
-    der_signature: Vec<u8>,
-) -> Result<Vec<u8>, SignatureError> {
-    let signature = p256::ecdsa::Signature::from_der(&der_signature).map_err(|e| {
-        SignatureError::InvalidSignature {
-            value: e.to_string(),
-        }
-    })?;
-    let mut session_manager = session_manager.inner.lock().unwrap();
-    session_manager
-        .submit_next_signature(signature.to_bytes().to_vec())
-        .map_err(|e| SignatureError::Generic {
-            value: format!("Could not submit next signature: {e:?}"),
-        })?;
-    session_manager
-        .retrieve_response()
-        .ok_or(SignatureError::TooManyDocuments)
+    #[error("no signature payload received from session manager")]
+    MissingSignature,
 }
 
 #[derive(thiserror::Error, uniffi::Error, Debug)]
 pub enum TerminationError {
     #[error("{value}")]
     Generic { value: String },
-}
-
-#[uniffi::export]
-fn terminate_session() -> Result<Vec<u8>, TerminationError> {
-    let msg = session::SessionData {
-        data: None,
-        status: Some(session::Status::SessionTermination),
-    };
-    let msg_bytes = serde_cbor::to_vec(&msg).map_err(|e| TerminationError::Generic {
-        value: format!("Could not serialize message bytes: {e:?}"),
-    })?;
-    Ok(msg_bytes)
 }
 
 #[derive(uniffi::Object)]
@@ -330,7 +350,11 @@ mod tests {
         },
         presentation::reader,
     };
-    use p256::ecdsa::signature::{SignatureEncoding, Signer};
+
+    use crate::{
+        local_key_manager::LocalKeyManager, local_store::LocalStore, Credential,
+        KeyManagerInterface, StorageManagerInterface,
+    };
 
     use super::*;
 
@@ -338,12 +362,35 @@ mod tests {
     fn end_to_end_ble_presentment() {
         let mdoc_b64 = include_str!("../../tests/res/mdoc.b64");
         let mdoc_bytes = BASE64_STANDARD.decode(mdoc_b64).unwrap();
-        let mdoc = MDoc::from_cbor(mdoc_bytes);
-        let key: p256::ecdsa::SigningKey =
-            p256::SecretKey::from_sec1_pem(include_str!("../../tests/res/sec1.pem"))
-                .unwrap()
-                .into();
-        let session_data = initialise_session(mdoc, Uuid::new_v4()).unwrap();
+        let mdoc_id = Uuid::new_v4();
+        let smi: Arc<dyn StorageManagerInterface> = Arc::new(LocalStore);
+        let key = p256::SecretKey::from_sec1_pem(include_str!("../../tests/res/sec1.pem")).unwrap();
+        let kmi: Arc<dyn KeyManagerInterface> =
+            Arc::new(LocalKeyManager::new_with_key("test_key".into(), key));
+
+        let wallet = Wallet::new(smi, kmi).unwrap();
+
+        let second_smi: Arc<dyn StorageManagerInterface> = Arc::new(LocalStore);
+
+        // Store the mdoc in the VDC Collection
+        wallet
+            .vdc_collection
+            .add(
+                Credential::new(
+                    mdoc_id,
+                    ClaimFormatDesignation::MsoMDoc,
+                    CredentialType::Iso18013_5_1mDl,
+                    mdoc_bytes,
+                ),
+                second_smi,
+            )
+            .unwrap();
+
+        // Start a new presentation session
+        let session = wallet
+            .initialize_mdl_presentation(&mdoc_id.to_string(), Uuid::new_v4())
+            .unwrap();
+
         let namespaces: device_request::Namespaces = [(
             "org.iso.18013.5.1".to_string(),
             [
@@ -366,13 +413,13 @@ mod tests {
         .unwrap();
         let (mut reader_session_manager, request, _ble_ident) =
             reader::SessionManager::establish_session(
-                session_data.qr_code_uri,
+                session.qr_code_uri.clone(),
                 namespaces.clone(),
                 Some(trust_anchor),
             )
             .unwrap();
         // let request = reader_session_manager.new_request(namespaces).unwrap();
-        let request_data = handle_request(session_data.state, request).unwrap();
+        let _request_data = session.handle_request(request).unwrap();
         let permitted_items = [(
             "org.iso.18013.5.1.mDL".to_string(),
             [(
@@ -384,11 +431,10 @@ mod tests {
         )]
         .into_iter()
         .collect();
-        let signing_payload =
-            submit_response(request_data.session_manager.clone(), permitted_items).unwrap();
-        let signature: p256::ecdsa::Signature = key.sign(&signing_payload);
-        let response =
-            submit_signature(request_data.session_manager, signature.to_der().to_vec()).unwrap();
+        let response = session
+            .submit_response(permitted_items, "test_key".into())
+            .unwrap();
+
         // Root cert is expired
         let mut errors = reader_session_manager.handle_response(&response).errors;
         let (k, v) = errors.pop_first().unwrap();
