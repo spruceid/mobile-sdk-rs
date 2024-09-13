ed0239a39f9efb8bd3f01330b52c51b398db9cb2
Jacob <jacob.ward@spruceid.com>
2024-09-13 17:18:26 +0100

    Refactor credential interfaces.
    Add better support for different credential formats.

---
+++ src/credential/json_vc.rs
@@ -0,0 +1,146 @@
+use std::sync::Arc;
+
+use serde_json::Value as Json;
+use ssi::{
+    claims::vc::{v1::Credential as _, v2::Credential as _},
+    prelude::AnyJsonCredential,
+};
+use uuid::Uuid;
+
+use crate::{CredentialType, KeyAlias};
+
+use super::{Credential, VcdmVersion};
+
+#[derive(uniffi::Object, Debug, Clone)]
+/// A verifiable credential secured as JSON.
+pub struct JsonVc {
+    id: Uuid,
+    raw: Json,
+    credential_string: String,
+    parsed: AnyJsonCredential,
+    key_alias: Option<KeyAlias>,
+}
+
+#[uniffi::export]
+impl JsonVc {
+    #[uniffi::constructor]
+    /// Construct a new credential from UTF-8 encoded JSON.
+    pub fn new_from_compact_jws(utf8_json_string: String) -> Result<Arc<Self>, JsonVcInitError> {
+        let id = Uuid::new_v4();
+        let json = serde_json::from_str(&utf8_json_string)
+            .map_err(|_| JsonVcInitError::JsonStringDecoding)?;
+        Self::from_json(id, json, None)
+    }
+
+    #[uniffi::constructor]
+    /// Construct a new credential from UTF-8 encoded JSON.
+    pub fn new_from_compact_jws_with_key(
+        utf8_json_string: String,
+        key_alias: KeyAlias,
+    ) -> Result<Arc<Self>, JsonVcInitError> {
+        let id = Uuid::new_v4();
+        let json = serde_json::from_str(&utf8_json_string)
+            .map_err(|_| JsonVcInitError::JsonStringDecoding)?;
+        Self::from_json(id, json, Some(key_alias))
+    }
+
+    /// The keypair identified in the credential for use in a verifiable presentation.
+    pub fn key_alias(&self) -> Option<KeyAlias> {
+        self.key_alias.clone()
+    }
+
+    /// The local ID of this credential.
+    pub fn id(&self) -> Uuid {
+        self.id
+    }
+
+    /// The version of the Verifiable Credential Data Model that this credential conforms to.
+    pub fn vcdm_version(&self) -> VcdmVersion {
+        match &self.parsed {
+            ssi::claims::vc::AnySpecializedJsonCredential::V1(_) => VcdmVersion::V1,
+            ssi::claims::vc::AnySpecializedJsonCredential::V2(_) => VcdmVersion::V2,
+        }
+    }
+
+    /// Access the W3C VCDM credential as a JSON encoded UTF-8 string.
+    pub fn credential_as_json_encoded_utf8_string(&self) -> String {
+        self.credential_string.clone()
+    }
+
+    /// The type of this credential. Note that if there is more than one type (i.e. `types()`
+    /// returns more than one value), then the types will be concatenated with a "+".
+    pub fn r#type(&self) -> CredentialType {
+        CredentialType(self.types().join("+"))
+    }
+
+    /// The types of the credential from the VCDM, excluding the base `VerifiableCredential` type.
+    pub fn types(&self) -> Vec<String> {
+        match &self.parsed {
+            ssi::claims::vc::AnySpecializedJsonCredential::V1(vc) => vc.additional_types().to_vec(),
+            ssi::claims::vc::AnySpecializedJsonCredential::V2(vc) => vc.additional_types().to_vec(),
+        }
+    }
+}
+
+impl JsonVc {
+    pub(crate) fn to_json_bytes(&self) -> Result<Vec<u8>, JsonVcEncodingError> {
+        serde_json::to_vec(&self.raw).map_err(|_| JsonVcEncodingError::JsonBytesEncoding)
+    }
+
+    fn from_json_bytes(
+        id: Uuid,
+        raw: Vec<u8>,
+        key_alias: Option<KeyAlias>,
+    ) -> Result<Arc<Self>, JsonVcInitError> {
+        let json = serde_json::from_slice(&raw).map_err(|_| JsonVcInitError::JsonBytesDecoding)?;
+        Self::from_json(id, json, key_alias)
+    }
+
+    fn from_json(
+        id: Uuid,
+        json: Json,
+        key_alias: Option<KeyAlias>,
+    ) -> Result<Arc<Self>, JsonVcInitError> {
+        let raw = json;
+
+        let parsed =
+            serde_json::from_value(raw.clone()).map_err(|_| JsonVcInitError::CredentialDecoding)?;
+
+        let credential_string = serde_json::to_string(&parsed)
+            .map_err(|_| JsonVcInitError::CredentialStringEncoding)?;
+
+        Ok(Arc::new(Self {
+            id,
+            raw,
+            credential_string,
+            parsed,
+            key_alias,
+        }))
+    }
+}
+
+impl TryFrom<Credential> for Arc<JsonVc> {
+    type Error = JsonVcInitError;
+
+    fn try_from(credential: Credential) -> Result<Self, Self::Error> {
+        JsonVc::from_json_bytes(credential.id, credential.payload, credential.key_alias)
+    }
+}
+
+#[derive(Debug, uniffi::Error, thiserror::Error)]
+pub enum JsonVcInitError {
+    #[error("failed to decode a W3C VCDM (v1 or v2) Credential from JSON")]
+    CredentialDecoding,
+    #[error("failed to encode the credential as a UTF-8 string")]
+    CredentialStringEncoding,
+    #[error("failed to decode JSON from bytes")]
+    JsonBytesDecoding,
+    #[error("failed to decode JSON from a UTF-8 string")]
+    JsonStringDecoding,
+}
+
+#[derive(Debug, uniffi::Error, thiserror::Error)]
+pub enum JsonVcEncodingError {
+    #[error("failed to encode JSON as bytes")]
+    JsonBytesEncoding,
+}
