4f63eb71664842cbb2ce9c67f7a476a8a23eaa1f
Ryan Tate <ryan.michael.tate@gmail.com>
2024-09-23 14:21:36 -0700

    update swift package, use local binary target for xcode testing
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     ],
     dependencies: [ ],
     targets: [
-        .binaryTarget(name: "RustFramework", url: "https://github.com/spruceid/mobile-sdk-rs/releases/download/0.0.30/RustFramework.xcframework.zip", checksum: "f20f14d7bea08cbc85b1f16dd5db48641ac7c0439bae5f57aa26d056cf9bab83"),
-        // .binaryTarget(name: "RustFramework", url: "https://github.com/spruceid/mobile-sdk-rs/releases/download/0.0.30/RustFramework.xcframework.zip", checksum: "f20f14d7bea08cbc85b1f16dd5db48641ac7c0439bae5f57aa26d056cf9bab83"),
+        //.binaryTarget(name: "RustFramework", url: "https://github.com/spruceid/mobile-sdk-rs/releases/download/0.0.30/RustFramework.xcframework.zip", checksum: "f20f14d7bea08cbc85b1f16dd5db48641ac7c0439bae5f57aa26d056cf9bab83"),
+        .binaryTarget(name: "RustFramework", path: "./MobileSdkRs/RustFramework.xcframework"),
         .target(
             name: "SpruceIDMobileSdkRs",
             dependencies: [
