c62401c5d1151ee3c0baae38646639f321cca4ba
Ryan Tate <ryan.michael.tate@gmail.com>
2024-09-23 13:38:18 -0700

    re-add oid4vci module, bump to ssi 0.9
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,11 @@ pub mod common;
 pub mod credential;
 pub mod local_store;
 pub mod mdl;
-// TODO: Update OID4VCI with latest changes to ssi 0.9
-// pub mod oid4vci;
+pub mod oid4vci;
 pub mod storage_manager;
 pub mod vdc_collection;
-pub mod w3c_vc_barcodes;
-// Verifier
 pub mod verifier;
+pub mod w3c_vc_barcodes;
 
 pub use common::*;
 pub use mdl::reader::*;
