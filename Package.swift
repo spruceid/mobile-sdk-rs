d559d750dbd5945eace315a05ce05b2702ed2865
Ryan Tate <ryan.michael.tate@gmail.com>
2024-09-24 13:22:23 -0700

    use 0.0.31 binary target release for swift
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- Package.swift
+++ Package.swift
@@ -18,7 +18,6 @@ let package = Package(
     ],
     dependencies: [ ],
     targets: [
-        //.binaryTarget(name: "RustFramework", url: "https://github.com/spruceid/mobile-sdk-rs/releases/download/0.0.31/RustFramework.xcframework.zip", checksum: "5b4dcf4d371ef773887e6f77a37b79d0389fb555147f308da0aaa39c12376651"),
         .binaryTarget(name: "RustFramework", url: "https://github.com/spruceid/mobile-sdk-rs/releases/download/0.0.31/RustFramework.xcframework.zip", checksum: "5b4dcf4d371ef773887e6f77a37b79d0389fb555147f308da0aaa39c12376651"),
         .target(
             name: "SpruceIDMobileSdkRs",
