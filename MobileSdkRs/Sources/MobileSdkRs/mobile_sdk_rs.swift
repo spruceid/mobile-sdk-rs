94e0d1bb5d2e5918d2ce75c2d898e55ebeb41bae
Ross Schulman <ross@rbs.io>
2024-09-30 10:57:39 -0400

    Generate swift

--- MobileSdkRs/Sources/MobileSdkRs/mobile_sdk_rs.swift
+++ MobileSdkRs/Sources/MobileSdkRs/mobile_sdk_rs.swift
@@ -7699,7 +7699,7 @@ private var initializationResult: InitializationResult = {
     if (uniffi_mobile_sdk_rs_checksum_func_initialize_mdl_presentation() != 29387) {
         return InitializationResult.apiChecksumMismatch
     }
-    if (uniffi_mobile_sdk_rs_checksum_func_initialize_mdl_presentation_from_bytes() != 64088) {
+    if (uniffi_mobile_sdk_rs_checksum_func_initialize_mdl_presentation_from_bytes() != 12482) {
         return InitializationResult.apiChecksumMismatch
     }
     if (uniffi_mobile_sdk_rs_checksum_func_oid4vci_exchange_credential() != 13827) {
