efc51de51b4363d105176030390fd36d1adff10a
Ryan Tate <ryan.tate@spruceid.com>
2024-12-20 10:19:39 -0800

    Fix/ios der encoding presentation signature (#69)
    Signed-off-by: Ryan Tate <ryan.tate@spruceid.com>
    Co-authored-by: Simon Bihel <simon.bihel@spruceid.com>
    Co-authored-by: Juliano Cezar Chagas Tavares <julianocezargyn@hotmail.com>

--- Package.swift
+++ Package.swift
@@ -2,13 +2,13 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // Swift Package: SpruceIDMobileSdkRs
 
-import PackageDescription;
+import PackageDescription
 
 let package = Package(
     name: "SpruceIDMobileSdkRs",
     platforms: [
         .iOS(.v14),
-        .macOS(.v10_15)
+        .macOS(.v10_15),
     ],
     products: [
         .library(
@@ -16,16 +16,19 @@ let package = Package(
             targets: ["SpruceIDMobileSdkRs"]
         )
     ],
-    dependencies: [ ],
+    dependencies: [],
     targets: [
-        .binaryTarget(name: "RustFramework", url: "https://github.com/spruceid/mobile-sdk-rs/releases/download/0.4.3/RustFramework.xcframework.zip", checksum: "bd273b22e74532c6bb2ff229e43083ad1df67c431b5e96dda64c505beba896b4"),
-        // .binaryTarget(name: "RustFramework", url: "https://github.com/spruceid/mobile-sdk-rs/releases/download/0.4.3/RustFramework.xcframework.zip", checksum: "bd273b22e74532c6bb2ff229e43083ad1df67c431b5e96dda64c505beba896b4"),
+        .binaryTarget(
+            name: "RustFramework",
+            url:
+                "https://github.com/spruceid/mobile-sdk-rs/releases/download/0.4.3/RustFramework.xcframework.zip",
+            checksum: "bd273b22e74532c6bb2ff229e43083ad1df67c431b5e96dda64c505beba896b4"),
         .target(
             name: "SpruceIDMobileSdkRs",
             dependencies: [
                 .target(name: "RustFramework")
             ],
             path: "./MobileSdkRs/Sources/MobileSdkRs"
-        )
+        ),
     ]
 )
